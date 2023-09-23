cfeb7e05dee1c126b87f6c55bf6044beee975f8c
Shan-Ul-Haq <shanulhaq84@gmail.com>
2023-09-23 18:33:58 +0500

    Initial commit

---
+++ atm.ts
@@ -0,0 +1,90 @@
+import chalk from "chalk";
+import inquirer from "inquirer";
+import chalkAnimation from "chalk-animation"
+
+const sleep = () => {
+    return new Promise((res) => {
+        setTimeout(res, 3000)
+    })
+}
+async function welcome() {
+    let rainbowTitle = chalkAnimation.rainbow("Welcome to Typescript ATM Made By Shan");
+    await sleep();
+    rainbowTitle.stop();
+}
+await welcome()
+
+
+interface ansType {
+    userId: string,
+    userPin: number,
+    accountType: string,
+    transactionType: string,
+    amount: number,
+    Balance: number,
+}
+
+let answer: ansType = await inquirer.prompt([
+    {
+    type:   "input",
+    name: "userId",
+    message: "Please enter your ID:"
+    },
+
+    {
+        type:   "number",
+        name: "userPin",
+        message: "Please enter your Password:"
+    },
+
+    {
+        type:   "list",
+        name: "accountType",
+        choices: ["Current Account", "Saving Account"],
+        message: "Please select your Type:",
+    },
+
+    {
+        type:   "list",
+        name: "transactionType",
+        choices: ["FastCash", "Withdraw", "Balance Inquiry"],
+        message: "Please select an Option:",
+        when(answer) {
+            return answer.accountType
+        },
+    },    
+
+    {
+        type:   "list",
+        name: "amount",                     // amount
+        choices: [500, 1000, 2000, 5000, 10000],
+        message: "Please select your Amount:",
+        when(answer) {
+            return answer.transactionType === "FastCash"
+        },
+    },
+
+    {
+        type:   "number",
+        name: "amount",                     // amount
+        message: "Enter your Amount:",
+        when(answer) {
+            return answer.transactionType === "Withdraw"
+        },
+    },
+])
+
+if(answer.userId && answer.userPin) {
+    let Balance = Math.floor (Math.random() * 1000000);
+    console.log(Balance);
+    let Enteredamount = answer.amount;
+if (Balance >= Enteredamount) {
+    let remaining = Balance - Enteredamount;
+    console.log ("Your remaining balance is: " + remaining);
+    } else {
+    console.log("insufficient balance");
+}
+}
+    
+ 
+console.log(answer);
